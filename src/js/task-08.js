5368f19c33dabac1048db9e6bafd468e2bd6b894
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-02-09 21:03:52 +0200

    all

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -2,25 +2,20 @@ const formEl = document.querySelector('form.login-form');
 
 formEl.addEventListener('submit', onFormSubmit);
 
+const formEl = document.querySelector('form.login-form')
+formEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
-    event.preventDefault();
-    const formElements = event.currentTarget.elements;
-    const email = formElements.email.value;
-    const password = formElements.password.value;
-    const formData = { email, password };
-    if (email && password) {
-        console.log(formData);
-    }
-    else {
-        alert('Пожалуйста, заполните все поля');
-    }
-    formData.forEach((value, name) => {
-        console.log('Значение свойства', value);
-        console.log('Имя поля', name)
-    });
-    if (email.value === "" || password.value === "") {
-        onFormSubmit.reset()
-     }
-};
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = { email, password };
+  if (email && password) {
+    event.currentTarget.reset();
+    console.log(formData);
+      } else {
+      alert('Please complete all fields')
+  }
+  }
 
 
