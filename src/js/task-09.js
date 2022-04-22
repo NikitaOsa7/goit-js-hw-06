52aed09f7ad960dccff6fbaa7ae5b9e4276da41e
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-04-22 12:19:48 +0300

    повторение

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,16 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
+
 const bodyEl = document.querySelector('body');
-const buttonEl = document.querySelector('button')
+const colorChangeButton = document.querySelector('.change-color');
 const spanEl = document.querySelector('span')
 
-buttonEl.addEventListener('click', onBodyColorChange);
 
-function onBodyColorChange (event) {
+colorChangeButton.addEventListener('click', onBodyColorChange);
+
+function onBodyColorChange(event) {
   const newColor = getRandomHexColor();
   bodyEl.style.backgroundColor = newColor;
-  spanEl.textContent = newColor;
+  spanEl.textContent = newColor
 }
-
 
